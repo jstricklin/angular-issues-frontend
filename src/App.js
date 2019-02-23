5f1cb79bc06f70c4d1edbe377d70703223525b41
Jose Stricklin <josestricklin@El-Diablo.local>
2019-02-22 23:17:14 -0700

    update main app component

--- src/App.js
+++ src/App.js
@@ -6,20 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <h1>This is placeholder content.</h1>
+          <p>Soon we will have a whole front end to display. For now though...</p>
       </div>
     );
   }
