7e7921ee3443eba5456d119a00f087131a56a6a6
Jose Stricklin <josestricklin@El-Diablo.local>
2019-02-23 20:43:36 -0700

    update styling for font sizes and prep for test deploy

---
+++ build/precache-manifest.f2fbee83ee4865b760d943e4d816e9ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d927cf1a6813fdb6c55f4f830ac2661e",
+    "url": "/static/media/angularjs-logo.d927cf1a.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c2b9bdf058fed573e719",
+    "url": "/static/js/main.c2b9bdf0.chunk.js"
+  },
+  {
+    "revision": "cd46b8ac1e2ffde43170",
+    "url": "/static/js/2.cd46b8ac.chunk.js"
+  },
+  {
+    "revision": "c2b9bdf058fed573e719",
+    "url": "/static/css/main.c65b9483.chunk.css"
+  },
+  {
+    "revision": "cd46b8ac1e2ffde43170",
+    "url": "/static/css/2.38bf2465.chunk.css"
+  },
+  {
+    "revision": "bcdc5cb9f4c783beaeb8c09927c7122c",
+    "url": "/index.html"
+  }
+];
--- src/components/IssuePage.js
+++ src/components/IssuePage.js
@@ -7,7 +7,7 @@ const IssuePage = (props) => {
     console.log('issue', issue);
     return (
         <div className='col-8 mx-auto pt-4'>
-            <div className='col-12 d-flex p-0 my-3 justify-content-between border-bottom'><h5>Issue ID: {id}</h5><h5>{issue ? issue.title : null}</h5></div>
+            <div className='col-12 d-flex p-0 pt-3 pb-3 my-3 justify-content-between border-bottom'><h5>Issue ID: {id}</h5><h5>{issue ? issue.title : null}</h5></div>
             { issue ? <Issue pathID={id} issue={issue} /> : <h1>Loading Issue...</h1> }
         </div>
     );
